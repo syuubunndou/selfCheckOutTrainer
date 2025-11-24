c5ccaecec969289d3149b5561bba5cf58e1703ee
syuubunndou <zin.syuubunndou@gmail.com>
2025-11-24 16:25:26 +0900

    Add files via upload

--- eaonCheckout.js
+++ eaonCheckout.js
@@ -2249,7 +2249,7 @@ class ScrOrnersCard {
 class AeonLikeCheckOutApp {
     constructor() {
         this.viceAudioList = [];
-        alert("16:12");
+        alert("16:23");
         this.AUDIO_MANAGER = new AudioManager();
         this.screenObject = new Scr1ChoosePayments(this.AUDIO_MANAGER);
         this.currentScreenName = this.screenObject.SCREEN_NAME;
